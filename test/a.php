7e78d26f660418f8bcfd360d797ee1335e9f1395
Kerem Güneş <k-gun@mail.com>
2017-03-19 06:46:10 +0300

    expr index..

--- test/a.php
+++ test/a.php
@@ -5,63 +5,16 @@ function isLetter($c) { return ($c >= 'a' && $c <= 'z') || ($c >= 'A' && $c <= '
 function isNumber($c) { return ($c >= '0' && $c <= '9'); }
 function isId($c) { return ($c === '_') || isLetter($c) || isNumber($c); }
 
-function parseBlockExpr($expr, $cStart, $cEnd) {
-    if (false === strpos($expr, $cEnd)) {
-        return $expr;
-    }
-
-    $expr = preg_split('~(.+\s*\\'. $cEnd .')\s*(,)?\s*(.+)?$~',
-        $expr, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE)[0];
-
-    $i = 0; $stack = []; $buffer = ''; $depth = 0;
-    while ('' !== ($c =@ $expr[$i++])) {
-        switch ($c) {
-            case $cStart:
-                $depth++;
-                break;
-            case ',':
-                if (!$depth) {
-                    if ($buffer !== '') {
-                        $stack[] = $buffer;
-                        $buffer = '';
-                    }
-                    continue 2;
-                }
-                break;
-            case ' ':
-                if (!$depth) {
-                    continue 2;
-                }
-                break;
-            case $cEnd:
-                if ($depth) {
-                    $depth--;
-                } else {
-                    $stack[] = $buffer . $c;
-                    $buffer = '';
-                    continue 2;
-                }
-                break;
-        }
-        $buffer .= $c;
-    }
-
-    if ($buffer !== '') {
-        $stack[] = $buffer;
-    }
-
-    return $stack;
-}
-
 function parseExpr($expr) {
     pre($expr);
     $stack = [];
-    $i = 0; $stack = []; $buffer = ''; $depth = 0;
+    $i = 0; $stack = []; $depth = 0; $buffer = ''; $bufferIndex = null;
     while ('' !== ($c =@ $expr[$i++])) {
         switch ($c) {
             case '"':
             case "'":
                 $buffer = $c;
+                $bufferIndex = $i - 1;
                 if ($c === '"') {
                     while (($cs =@ $expr[$i++]) !== '' && ($cs !== '"' || $cs === '\\')) {
                         $buffer .= $cs;
@@ -81,36 +34,40 @@ function parseExpr($expr) {
                 }
                 $buffer .= $c;
                 break;
-            // case '[':
-            //     $buffer = parseBlockExpr(substr($expr, $i-1), '[', ']')[0];
-            //     $i = $i + strlen($buffer) - 1;
-            //     break;
+            case '?': // ?: and ?? expressions
+                $buffer = $c;
+                $bufferIndex = $i - 1;
+                if ($expr[$i] === ':' || $expr[$i] === '?') {
+                    $buffer .= $expr[$i];
+                    $i++;
+                }
+                break;
             case ' ':
                 if (!$depth) {
                     continue 2;
                 }
                 break;
-            case isId($c): // id leri yakalamiyor
+            case isId($c):
                 $buffer = $c;
                 while (isId($cs =@ $expr[$i])) {
                     $buffer .= $cs;
                     $i++;
                 }
-                $stack[] = $buffer;
+                $stack[] = [$buffer, $i - 1];
                 $buffer = '';
                 break;
             default:
                 if ($depth) {
                     $depth--;
                 } else {
-                    $stack[] = $c;
+                    $stack[] = [$c, $i - 1];
                     $buffer = '';
                     continue 2;
                 }
         }
 
         if ($buffer !== '') {
-            $stack[] = $buffer;
+            $stack[] = [$buffer, $bufferIndex];
         }
     }
 
@@ -128,7 +85,8 @@ $expr = "1, ' '";
 // $expr = '"1..1\"..", 1, []';
 // $expr = '1,[1,2,3,[4,"5",["aşkaşlka"]]], 1, "1111", foo("1", a)';
 // $expr = 'foo("aa"), 1, "str", ["a"]';
-$expr = '1, [aa, "aa", [1]]';
+// $expr = '1, [aa, "aa", [a??1]]';
+$expr = '"1", a';
 
 $tokens = parseExpr($expr);
 pre($tokens);
